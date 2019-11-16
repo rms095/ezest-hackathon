2a435527549967ae7da28843ccaaf04654ff11ad
Rohit More <deepakmore@outlook.com>
2019-11-16 12:17:32 +0530

    base project

---
+++ MyFootprint/src/main/java/com/ezest/hackathon/NoMiddleMen/NoMiddleMenApplication.java
@@ -0,0 +1,13 @@
+package com.ezest.hackathon.NoMiddleMen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoMiddleMenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoMiddleMenApplication.class, args);
+	}
+
+}
---
+++ MyFootprint/src/test/java/com/ezest/hackathon/NoMiddleMen/NoMiddleMenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezest.hackathon.NoMiddleMen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoMiddleMenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
